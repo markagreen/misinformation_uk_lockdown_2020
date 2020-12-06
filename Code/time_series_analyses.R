2b580de9569d820710a1b298290de4fee855c883
Mark A Green <mark.green@liverpool.ac.uk>
2020-12-06 15:50:17 +0000

    Added topic model trend analysis

--- Code/time_series_analyses.R
+++ Code/time_series_analyses.R
@@ -369,3 +369,140 @@ checkresiduals(model3) # Check for auto-correlation
 # ggplot(my.fitted, aes(x = trend, y = pred)) + geom_line() # Plot segmented model
 # ggplot(tw_hour, aes(x = trend, y = tweets)) + geom_line() + geom_line(data = my.fitted, aes(x = trend, y = pred), colour = "tomato") # Plot segmented model on top of actual data
 
+
+### Part 5: Interrupted time series of topic models ###
+
+
+# Load data
+topics_long <- read.csv("./Data/topics_per_hour.csv")
+topics_long$hour <- ymd_hms(topics_long$hour) 
+
+# Calculate offset (count of all misinformation tweets)
+dt <- data.table(topics_long)
+dt <- dt[, list(total = sum(n, na.rm=TRUE)), by = "hour"] # Aggregate to get total
+topics_long <- merge(topics_long, dt, by = "hour", all.x = TRUE) # Join back on
+
+# Define intervention
+topics_long$intervention[topics_long$hour < "2020-03-23 20:00:00"] <- 0
+topics_long$intervention[topics_long$hour >= "2020-03-23 20:00:00"] <- 1
+
+# Split by topic
+topic1 <- topics_long[topics_long$topic == 1,]
+topic2 <- topics_long[topics_long$topic == 2,]
+topic3 <- topics_long[topics_long$topic == 3,]
+topic4 <- topics_long[topics_long$topic == 4,]
+
+# Method 1: Causal Impact Model (Bayesian Structural Time Series and Counterfactual Estimation)
+
+# Define intervention period
+pre.period <- ymd_hms(c("2020-03-21 20:00:00", "2020-03-23 19:00:00")) # Periods must not overlap
+post.period <- ymd_hms(c("2020-03-23 20:00:00", "2020-03-25 20:00:00"))
+
+# Topic 1 #
+
+# Tidy data
+for_model <- zoo(cbind(topic1$n, topic1$total), topic1$hour) # Left cbind in here if want to add control variables
+colnames(for_model) = c("y", "x1") # Rename columns (if more than one use c("y", "x1", etc) if only one then "y")
+
+# Run analysis
+model_t1 <- CausalImpact(for_model, pre.period, post.period) # Model
+plot(model_t1) # Plot results
+summary(model_t1) # Get summary statistics
+
+# Topic 2 #
+
+# Tidy data
+for_model <- zoo(cbind(topic2$n, topic2$total), topic2$hour) # Left cbind in here if want to add control variables
+colnames(for_model) = c("y", "x1") # Rename columns (if more than one use c("y", "x1", etc) if only one then "y")
+
+# Run analysis
+model_t2 <- CausalImpact(for_model, pre.period, post.period) # Model
+plot(model_t2) # Plot results
+summary(model_t2) # Get summary statistics
+
+# Topic 3 #
+
+# Tidy data
+for_model <- zoo(cbind(topic3$n, topic3$total), topic3$hour) # Left cbind in here if want to add control variables
+colnames(for_model) = c("y", "x1") # Rename columns (if more than one use c("y", "x1", etc) if only one then "y")
+
+# Run analysis
+model_t3 <- CausalImpact(for_model, pre.period, post.period) # Model
+plot(model_t3) # Plot results
+summary(model_t3) # Get summary statistics
+
+# Topic 4 #
+
+# Tidy data
+for_model <- zoo(cbind(topic4$n, topic4$total), topic4$hour) # Left cbind in here if want to add control variables
+colnames(for_model) = c("y", "x1") # Rename columns (if more than one use c("y", "x1", etc) if only one then "y")
+
+# Run analysis
+model_t4 <- CausalImpact(for_model, pre.period, post.period) # Model
+plot(model_t4) # Plot results
+summary(model_t4) # Get summary statistics
+
+
+# Method 2: Poisson Regression Model
+
+# Misinformation - topic 1
+model2_t1 <- glm(n ~ offset(log(total)) + intervention + hour + harmonic(hour,2,24), family=quasipoisson, topic1)
+summary(model2_t1)
+exp(coef(model2_t1))
+exp(confint(model2_t1))
+
+# Misinformation - topic 2
+model2_t2 <- glm(n ~ offset(log(total)) + intervention + hour + harmonic(hour,2,24), family=quasipoisson, topic2)
+summary(model2_t2)
+exp(coef(model2_t2))
+exp(confint(model2_t2))
+
+# Misinformation - topic 3
+model2_t3 <- glm(n ~ offset(log(total)) + intervention + hour + harmonic(hour,2,24), family=quasipoisson, topic3)
+summary(model2_t3)
+exp(coef(model2_t3))
+exp(confint(model2_t3))
+
+# Misinformation - topic 4
+model2_t4 <- glm(n ~ offset(log(total)) + intervention + hour + harmonic(hour,2,24), family=quasipoisson, topic4)
+summary(model2_t4)
+exp(coef(model2_t4))
+exp(confint(model2_t4))
+
+# Method 3: ARIMA (time-series regression)
+
+# Topic 1
+preds <- cbind(topic1$total, topic1$intervention) # Define predictor variables
+colnames(preds) <- c("total", "intervention")
+model3_t1 <- auto.arima(topic1$n, xreg = preds) # Fit best fitting time series model
+model3_t1 # Print model
+coeftest(model3_t1) # Grab coefficients and model summary
+confint(model3_t1) # Confidence Intervals
+checkresiduals(model3_t1) # Check for auto-correlation
+
+# Topic 2
+preds <- cbind(topic2$total, topic2$intervention) # Define predictor variables
+colnames(preds) <- c("total", "intervention")
+model3_t2 <- auto.arima(topic2$n, xreg = preds) # Fit best fitting time series model
+model3_t2 # Print model
+coeftest(model3_t2) # Grab coefficients and model summary
+confint(model3_t2) # Confidence Intervals
+checkresiduals(model3_t2) # Check for auto-correlation
+
+# Topic 3
+preds <- cbind(topic3$total, topic3$intervention) # Define predictor variables
+colnames(preds) <- c("total", "intervention")
+model3_t3 <- auto.arima(topic3$n, xreg = preds) # Fit best fitting time series model
+model3_t3 # Print model
+coeftest(model3_t3) # Grab coefficients and model summary
+confint(model3_t3) # Confidence Intervals
+checkresiduals(model3_t3) # Check for auto-correlation
+
+# Topic 4
+preds <- cbind(topic4$total, topic4$intervention) # Define predictor variables
+colnames(preds) <- c("total", "intervention")
+model3_t4 <- auto.arima(topic4$n, xreg = preds) # Fit best fitting time series model
+model3_t4 # Print model
+coeftest(model3_t4) # Grab coefficients and model summary
+confint(model3_t4) # Confidence Intervals
+checkresiduals(model3_t4) # Check for auto-correlation
